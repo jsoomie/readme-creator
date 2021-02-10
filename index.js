43fc2188b40caf17dee3a575e7e6b79bc9a79f94
Jonathan Soukaseume <sksmejn@gmail.com>
2021-02-10 11:28:39 -0800

    added questions in the inquirer

--- index.js
+++ index.js
@@ -4,7 +4,42 @@ const inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "Enter project's name: "
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Enter author's name: "
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter project's GitHub URL: "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter how to use the project: "
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Enter project licenses: "
+    },
+    {
+        type: "input",
+        name: "credit",
+        message: "Enter needed credit for project: "
+    },
+    
 ];
 
 // function to write README file
